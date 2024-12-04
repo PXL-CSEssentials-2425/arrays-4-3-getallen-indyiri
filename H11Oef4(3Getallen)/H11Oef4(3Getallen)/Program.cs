4c757d7f0a4b145823e8ff891336f218173fdf72
Indy Iri <indy.iri@student.pxl.be>
2024-12-04 16:22:01 +0100

    Afgewerkt

--- H11Oef4(3Getallen)/H11Oef4(3Getallen)/Program.cs
+++ H11Oef4(3Getallen)/H11Oef4(3Getallen)/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool isValid = false;
+
+double[] numbers = new double[3];
+
+double sum = 0.0;
+
+do 
+{
+    Console.WriteLine("=== Som van 3 getallen bepalen ===");
+    Console.WriteLine();
+    Console.Write("Geef 1ste getal: ");
+    string inputFirstNumber = Console.ReadLine();
+    Console.WriteLine();
+    Console.Write("Geef 2de getal: ");
+    string inputSecondNumber = Console.ReadLine();
+    Console.WriteLine();
+    Console.Write("Geef 3de getal: ");
+    string inputThirdNumber = Console.ReadLine();
+    Console.WriteLine();
+
+    bool isFirstNumberValid = double.TryParse(inputFirstNumber, out numbers[0]);
+    bool isSecondNumberValid = double.TryParse(inputSecondNumber, out numbers[1]);
+    bool isThirdNumberValid = double.TryParse(inputThirdNumber, out numbers[2]);
+
+    if (!isFirstNumberValid || !isSecondNumberValid || !isThirdNumberValid || numbers[0] == 0.0 || numbers[1] == 0.0 || numbers[2] == 0.0)
+    {
+        Console.WriteLine("U moet getallen ingeven die niet gelijk zijn aan 0!");
+        Console.ReadKey(true);
+        Console.Clear();
+    }
+    else
+    {
+        foreach (double number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"De som is {sum}");
+        isValid = true;
+    }
+}
+while (isValid == false);
